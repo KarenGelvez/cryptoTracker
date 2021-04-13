7adee017672507983b4685c39d0e460f6395adb5
KarenGelvez <karen.gelvez2@unipamplona.edu.co>
2021-04-13 16:16:14 -0500

    [ADD] nuevos colores

--- src/res/colors.js
+++ src/res/colors.js
@@ -1,8 +1,10 @@
 const Colors = {
   blackPearl: '#20252c',
+  carmine: '#ef6372',
   charade: '#272c35',
-  zircon: '#e0e0e0',
+  picton: '#3c6fc8',
   white: '#fff',
+  zircon: '#e0e0e0',
 };
 
 export default Colors;
